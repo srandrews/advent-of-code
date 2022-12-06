9d5c6bf8ec048f62b8f625a542a62180b965fa8c
Simon Andrews <simon.andrews@globalrelay.net>
2022-12-06 13:22:41 +0000

    Day 6

---
+++ src/test/java/uk/co/eandrews/advent2022/Day06Test.java
@@ -0,0 +1,38 @@
+package uk.co.eandrews.advent2022;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import uk.co.eandrews.advent2022.day.day05.CraneOperation;
+import uk.co.eandrews.advent2022.day.day05.CraneOperationInputParser;
+import uk.co.eandrews.advent2022.day.day05.Day5;
+import uk.co.eandrews.advent2022.day.day06.Day6;
+import uk.co.eandrews.util.io.input.parser.InputParser;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Day06Test {
+
+    @ParameterizedTest
+    @CsvSource({"bvwbjplbgvbhsrlpgdmjqwftvncz,5", "nppdvjthqldpwncqszvftbrmjlhg,6",
+            "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg,10", "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw,11"})
+    void partOneSolution(String input, int expected) {
+        final InputParser<Stream<String>> parser = inputResolver -> input.lines();
+        final Day6 day6 = new Day6(parser);
+        assertThat(day6.partOneSolution().solve(parser.parse(input.lines()))).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"mjqjpqmgbljsphdztnvjfqwrcgsmlb,19", "bvwbjplbgvbhsrlpgdmjqwftvncz,23",
+            "nppdvjthqldpwncqszvftbrmjlhg,23", "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg,29",
+            "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw, 26"})
+    void partTwoSolution_test(String input, int expected) {
+        final InputParser<Stream<String>> parser = inputResolver -> input.lines();
+
+        final Day6 day6 = new Day6(parser);
+        assertThat(day6.partTwoSolution().solve(parser.parse(input.lines()))).isEqualTo(expected);
+    }
+
+}
