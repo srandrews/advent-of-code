ef1557646c8908c52a08bee45310b9c3efefdf7f
Simon Andrews <simon@eandrews.co.uk>
2022-12-08 21:33:05 +0000

    Day 8

--- src/main/java/uk/co/eandrews/advent2022/day/day08/Day8.java
+++ src/main/java/uk/co/eandrews/advent2022/day/day08/Day8.java
@@ -1,19 +1,19 @@
 package uk.co.eandrews.advent2022.day.day08;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import lombok.experimental.ExtensionMethod;
-import org.springframework.stereotype.Component;
-
 import one.util.streamex.StreamEx;
+import org.springframework.stereotype.Component;
 import uk.co.eandrews.advent2022.Day2022;
 import uk.co.eandrews.util.PuzzleSolution;
 import uk.co.eandrews.util.StreamExUtil;
 import uk.co.eandrews.util.io.input.parser.InputParser;
 
-@Component("Da87-2022")
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Component("Day8-2022")
 @ExtensionMethod({StreamEx.class, StreamExUtil.class})
 public class Day8 extends Day2022<Integer[][], Long> {
 
@@ -34,46 +34,51 @@ public class Day8 extends Day2022<Integer[][], Long> {
 
             Set<String> visible = new HashSet<>();
 
-            int maxHeight=0;
+            int maxHeight=-1;
 
             for (int i=0; i<input.length; i++) {
                 for (int j = 0; j<input[i].length; j++){
-                    if (input[i][j]>maxHeight) {
-                        maxHeight = input[i][j];
+                    int tree = input[i][j];
+                    if (tree>maxHeight) {
+                        maxHeight = tree;
                         visible.add("("+i+","+j+")");
                     }
                 }
-                maxHeight=0;
-            }
-
-            for (int i=input.length-1; i>=0; i--) {
-                for (int j = 0; j<input[i].length; j++){
-                    if (input[i][j]>maxHeight) {
-                        maxHeight = input[i][j];
-                        visible.add("("+i+","+j+")");
-                    }
-                }
-                maxHeight=0;
+                maxHeight=-1;
             }
 
             for (int i=0; i<input.length; i++) {
-                for (int j=input[i].length-1; j>=0; j--) {
-                    if (input[i][j]>maxHeight) {
-                        maxHeight = input[i][j];
+                for (int j = input[i].length-1; j>=0; j--){
+                    int tree = input[i][j];
+                    if (tree>maxHeight) {
+                        maxHeight = tree;
                         visible.add("("+i+","+j+")");
                     }
                 }
-                maxHeight=0;
+                maxHeight=-1;
             }
 
-            for (int i=input.length-1; i>=0; i--) {
-                for (int j=input[i].length-1; j>=0; j--) {
-                    if (input[i][j]>maxHeight) {
-                        maxHeight = input[i][j];
+
+            for (int j = 0; j<input[0].length; j++){
+                for (int i=0; i<input.length; i++) {
+                    int tree = input[i][j];
+                    if (tree>maxHeight) {
+                        maxHeight = tree;
                         visible.add("("+i+","+j+")");
                     }
                 }
-                maxHeight=0;
+                maxHeight=-1;
+            }
+
+            for (int j = 0; j<input[0].length; j++){
+                for (int i = input.length-1; i>=0; i--){
+                    int tree = input[i][j];
+                    if (tree>maxHeight) {
+                        maxHeight = tree;
+                        visible.add("("+i+","+j+")");
+                    }
+                }
+                maxHeight=-1;
             }
 
             return Long.valueOf(visible.size());
@@ -81,7 +86,98 @@ public class Day8 extends Day2022<Integer[][], Long> {
     }
 
     public PuzzleSolution<Integer[][], Long> partTwoSolution() {
-        return input -> 0L;
+        return input -> {
+            int maxScenicScore=0;
+
+            for (int i = 0; i<input.length; i++) {
+                for (int j = 0; j<input[i].length; j++) {
+                    int scenicScore = getScenicScore(i,j,input);
+                    if (scenicScore>maxScenicScore) {
+                        maxScenicScore = scenicScore;
+                    }
+                }
+            }
+            return Long.valueOf(maxScenicScore);
+        };
     }
+
+
+
+    private int getScenicScore(final int x, final int y, Integer[][] trees) {
+
+        int treeHeight=trees[x][y];
+        int tmpX = x+1;
+
+        int scenicScore=1;
+
+        int view = 0;
+
+        while (tmpX<trees.length) {
+
+            int nextTreeHeight = trees[tmpX][y];
+            view++;
+
+            if (nextTreeHeight>=treeHeight) {
+                break;
+            }
+
+            tmpX++;
+        }
+
+        scenicScore=scenicScore*view;
+
+        tmpX = x-1;
+        view = 0;
+
+        while (tmpX>=0) {
+
+            int nextTreeHeight = trees[tmpX][y];
+            view++;
+
+            if (nextTreeHeight>=treeHeight) {
+                break;
+            }
+
+            tmpX--;
+        }
+
+        scenicScore=scenicScore*view;
+
+        int tmpY = y+1;
+        view = 0;
+
+        while (tmpY<trees[0].length) {
+
+            int nextTreeHeight = trees[x][tmpY];
+            view++;
+
+            if (nextTreeHeight>=treeHeight) {
+                break;
+            }
+
+            tmpY++;
+        }
+
+        scenicScore=scenicScore*view;
+
+        tmpY = y-1;
+        view = 0;
+
+        while (tmpY>=0) {
+            int nextTreeHeight = trees[x][tmpY];
+            view++;
+
+            if (nextTreeHeight>=treeHeight) {
+                break;
+            }
+
+            tmpY--;
+        }
+
+        scenicScore=scenicScore*view;
+
+        return scenicScore;
+    }
+
 
 }
--- src/test/java/uk/co/eandrews/advent2022/Day08Test.java
+++ src/test/java/uk/co/eandrews/advent2022/Day08Test.java
@@ -1,10 +1,6 @@
 package uk.co.eandrews.advent2022;
 
 import org.junit.jupiter.api.Test;
-
-import uk.co.eandrews.advent2022.day.day07.Day7;
-import uk.co.eandrews.advent2022.day.day07.Directory;
-import uk.co.eandrews.advent2022.day.day07.DirectoryOperationsInputParser;
 import uk.co.eandrews.advent2022.day.day08.Day8;
 import uk.co.eandrews.advent2022.day.day08.TreeInputParser;
 import uk.co.eandrews.util.io.input.parser.InputParser;
@@ -28,11 +24,11 @@ class Day08Test {
         final Day8 day8 = new Day8(parser);
         assertThat(day8.partOneSolution().solve(parser.parse(input.lines()))).isEqualTo(21);
     }
-//
-//    @Test
-//    void partTwoSolution_should_return_24933642() {
-//        final Day7 day7 = new Day7(parser);
-//        assertThat(day7.partTwoSolution().solve(parser.parse(input.lines()))).isEqualTo(24933642);
-//    }
+
+    @Test
+    void partTwoSolution_should_return_8() {
+        final Day8 day8 = new Day8(parser);
+        assertThat(day8.partTwoSolution().solve(parser.parse(input.lines()))).isEqualTo(8);
+    }
 
 }
